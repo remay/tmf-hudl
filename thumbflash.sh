b1f03707a1f049ac6bf3fa405ece246ce5d1c659
Robert May <83421+remay@users.noreply.github.com>
2020-10-09 22:27:41 +0100

    re-organise the script to flash a thumbdrive

--- thumbflash.sh
+++ thumbflash.sh
@@ -2,11 +2,13 @@
 
 # flash our disk image to a usb stick
 
-test "$#" -eq 1 || { cat << __EOF__
+test "$#" -eq 2 || { cat << __EOF__
 
-usage:    write.sh /dev/sdN
+usage:    thumbflash.sh <file> <device>
+e.g:      thumbflash.sh thumb.img /dev/sdb
 
-write disk image to (unmounted) flash drive.
+write disk image to (unmounted) (flash) drive.
+Take care to provide the right device - this can be destructive
 
 __EOF__
 exit
@@ -15,9 +17,9 @@ exit
 # Needs to be run as root so we are allowed to mount and set other permissions
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
-   echo "Try re-running it as 'sudo $0'" 1>&2
+   echo "Try re-running it as 'sudo $0 $1 $2'" 1>&2
    exit 1
 fi
 
 # TODO - put a check in here as this next command can be destructive of the wrong device is supplied
-dd bs=4M if=tmf-hudl-thumb-drive.img of="$1" conv=fdatasync status=progress
+dd bs=4M if="$1" of="$2" conv=fdatasync status=progress
